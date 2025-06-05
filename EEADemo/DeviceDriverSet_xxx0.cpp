5171ff46303215416fbcd089b3d3bdf99255f8c5
Chris Williams <christopherwilliams@georgiasouthern.edu>
2025-06-05 18:59:32 -0400

    Add in additional sesnors

--- EEADemo/DeviceDriverSet_xxx0.cpp
+++ EEADemo/DeviceDriverSet_xxx0.cpp
@@ -1,7 +1,52 @@
 #include "DeviceDriverSet_xxx0.h"
 #include <arduino.h>
-/*Servo*/
 
+/*ULTRASONIC*/
+//#include <NewPing.h>
+// NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+void DeviceDriverSet_ULTRASONIC::DeviceDriverSet_ULTRASONIC_Init(void)
+{
+  pinMode(ECHO_PIN, INPUT); //Ultrasonic module initialization
+  pinMode(TRIG_PIN, OUTPUT);
+}
+void DeviceDriverSet_ULTRASONIC::DeviceDriverSet_ULTRASONIC_Get(uint16_t *ULTRASONIC_Get /*out*/)
+{
+  unsigned int tempda_x = 0;
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  tempda_x = ((unsigned int)pulseIn(ECHO_PIN, HIGH) / 58);
+  *ULTRASONIC_Get = tempda_x;
+  // sonar.ping() / US_ROUNDTRIP_CM; // Send ping, get ping time in microseconds (uS).
+}
+
+void DeviceDriverSet_ULTRASONIC::DeviceDriverSet_ULTRASONIC_Test(void)
+{
+
+  unsigned int tempda = 0;
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  tempda = ((unsigned int)pulseIn(ECHO_PIN, HIGH) / 58);
+
+  // if (tempda_x > 50)
+  // {
+  //   tempda_x = 50;
+  // }
+
+  // // return tempda;
+  // return tempda_x;
+
+  Serial.print("ULTRASONIC=");
+  Serial.print(tempda); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+  Serial.println("cm");
+}
+
+/*Servo*/
 Servo myservo; // create servo object to control a servo
 void DeviceDriverSet_Servo::DeviceDriverSet_Servo_Init(unsigned int Position_angle)
 {
--- EEADemo/EEADemo.ino
+++ EEADemo/EEADemo.ino
@@ -2,33 +2,53 @@
 #include "DeviceDriverSet_xxx0.h"
 #include "ApplicationFunctionSet_xxx0.cpp"
 #include "FastLED.h"
+#include "MPU6050.h"
+#include "MPU6050_getdata.h"
 
 // LED setup
 #define PIN_RBGLED 4
 #define NUM_LEDS 1
 CRGB leds[NUM_LEDS];
 
-// motors and servo setup
+// battery setup
+#define VOL_MEASURE_PIN A3
+unsigned long vol_measure_time = 0;
+
+// gyro, motors, servo, ultrasonic setup
+MPU6050_getdata AppMPU6050getdata;
 DeviceDriverSet_Motor AppMotor;
 Application_xxx Application_SmartRobotCarxxx0;
 DeviceDriverSet_Servo AppServo;
+DeviceDriverSet_ULTRASONIC myUltrasonic;
 
 void setup() {
   // initialize LED on main board
   Serial.begin(9600);
   FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
   FastLED.setBrightness(20);
-  
+
+  // init ultrasonic
+  myUltrasonic.DeviceDriverSet_ULTRASONIC_Init();
+
+  // init gyro
+  AppMPU6050getdata.MPU6050_dveInit();
+  delay(1000);
+  AppMPU6050getdata.MPU6050_calibration();
+
+  // init battery read
+  voltageInit();
+
   // start and initialize motor
   AppMotor.DeviceDriverSet_Motor_Init();
-  
+
   // wait 1 seconds
   delay(1000);
 
-  // move in all directions
+  // move car back and forth to verify movement
+  int motorSpeed = 50; // change this value to speed up or slow down car, min 0, max of 255
   for (Application_SmartRobotCarxxx0.Motion_Control = 0; Application_SmartRobotCarxxx0.Motion_Control < 9; Application_SmartRobotCarxxx0.Motion_Control = Application_SmartRobotCarxxx0.Motion_Control + 1)
   {
-    ApplicationFunctionSet_SmartRobotCarMotionControl(Application_SmartRobotCarxxx0.Motion_Control /*direction*/, 200 /*speed*/);
+    ApplicationFunctionSet_SmartRobotCarMotionControl(Application_SmartRobotCarxxx0.Motion_Control /*direction*/, motorSpeed /*speed*/);
     delay(1000); // wait 1 second
   }
 
@@ -46,8 +66,28 @@ void setup() {
   {
     AppServo.DeviceDriverSet_Servo_control(i);
   }
+
+  // change color of LED
+  myColor();
+
 }
 
+void loop() {
+  // measure voltage
+  voltageMeasure();
+
+  // print yaw
+  static float Yaw;
+  AppMPU6050getdata.MPU6050_dveGetEulerAngles(&Yaw);
+  Serial.print("Yaw:");
+  Serial.println(Yaw);
+
+  // check ultrasonic
+  myUltrasonic.DeviceDriverSet_ULTRASONIC_Test();
+  delay(500);
+}
+
+// LED functions
 uint32_t Color(uint8_t r, uint8_t g, uint8_t b)
 {
   return (((uint32_t)r << 16) | ((uint32_t)g << 8) | b);
@@ -55,36 +95,57 @@ uint32_t Color(uint8_t r, uint8_t g, uint8_t b)
 
 void myColor()
 {
-  int r=255,g=0,b=0;
-    for(int i=0;i<255;i++)
-    {
-      FastLED.showColor(Color(r, g, b));
-      r-=1;
-      g+=1;
-      delay(10);
-    }
-    r=0,g=0,b=255;
-    
-    for(int i=0;i<255;i++)
-    {
-      FastLED.showColor(Color(r, g, b));
-      r+=1;
-      b-=1;
-      delay(10);
-    }
-    r=0,g=255,b=0;
+  int r = 255, g = 0, b = 0;
+  for (int i = 0; i < 255; i++)
+  {
+    FastLED.showColor(Color(r, g, b));
+    r -= 1;
+    g += 1;
+    delay(10);
+  }
+  r = 0, g = 0, b = 255;
 
-    for(int i=0;i<255;i++)
-    {
-      FastLED.showColor(Color(r, g, b));
-      g-=1;
-      b+=1;
-      delay(10);
-    }
-    r=0,g=0,b=0;
+  for (int i = 0; i < 255; i++)
+  {
+    FastLED.showColor(Color(r, g, b));
+    r += 1;
+    b -= 1;
+    delay(10);
+  }
+  r = 0, g = 255, b = 0;
+
+  for (int i = 0; i < 255; i++)
+  {
+    FastLED.showColor(Color(r, g, b));
+    g -= 1;
+    b += 1;
+    delay(10);
+  }
+  r = 0, g = 0, b = 0;
 }
 
-void loop() {
-  // change color of LED
-  myColor();
+// Voltage functions below
+void voltageInit()
+{
+  pinMode(VOL_MEASURE_PIN, INPUT);
+}
+
+void voltageMeasure()
+{
+  if (millis() - vol_measure_time > 1000) //Measured every 1000 milliseconds
+  {
+    vol_measure_time = millis();
+    float voltage = (analogRead(VOL_MEASURE_PIN) * 5 ) * ((10 + 1.5) / 1.5) / 1024; //Read voltage value
+    //float voltage = (analogRead(VOL_MEASURE_PIN) * 0.0375);
+    voltage = voltage + (voltage * 0.08);
+    Serial.print("Current voltage value : ");
+    Serial.println(voltage);
+    float diff = 5 - voltage;
+    if (diff > 1.0)
+      Serial.println("Voltage is low, should be 5v");
+    else if (diff < -1.0)
+      Serial.println("Voltage is high, should be 5v");
+    else
+      Serial.println("Voltage is fine");
+  }
 }
